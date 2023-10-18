278acc07a5fc0f74486f6f4c9ebe4d4467cea3e6
dhirajmishra98 <gobindmishra23@gmail.com>
2023-10-18 09:57:44 +0530

    recursion standard problems

---
+++ Algorithms/Recursion Standard Problems/ReverseArray.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class ReverseArray{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = {1,2,3,4,5};
+        
+        sc.close();
+    }
+}
